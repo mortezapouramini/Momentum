8937b014a3eeace2e9d7f9effc06021a9d32b4ea
mortezapouramini <mortezapouramini2323@example.com>
2026-07-03 17:16:49 +0330

    refactor : shared cookie options in verifyEmail controller

--- src/config/cookie-config.js
+++ src/config/cookie-config.js
@@ -5,5 +5,12 @@ export const cookieOptions = {
         sameSite: "lax",
         maxAge: 5 * 60 * 1000,
         path : '/api/v1/auth/verify-email'
+    },
+    refreshToken : {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        path : '/api/v1/auth/refresh-token'
     }
 }

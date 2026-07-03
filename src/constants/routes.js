d1fa7989dcef89699df74a42f71be9b953ab73a1
mortezapouramini <mortezapouramini2323@example.com>
2026-07-03 10:53:24 +0330

    fix : export ROUTES in constants/routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,9 +1,11 @@
 const ROUTES = {
-    AUTH: {
-        REGISTER: "/api/v1/auth/register",
-        VERIFY_EMAIL: "/api/v1/auth/verify-email",
-        LOGIN: "/api/v1/auth/login",
-        LOGOUT: "/api/v1/auth/logout",
-        REFRESH_TOKEN: "/api/v1/auth/refresh-token",
-    }
-}
+  AUTH: {
+    REGISTER: "/api/v1/auth/register",
+    VERIFY_EMAIL: "/api/v1/auth/verify-email",
+    LOGIN: "/api/v1/auth/login",
+    LOGOUT: "/api/v1/auth/logout",
+    REFRESH_TOKEN: "/api/v1/auth/refresh-token",
+  },
+};
+
+module.exports = ROUTES;

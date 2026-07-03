03bc87a4e2d47b71302eb2b736c54accdada821b
mortezapouramini <mortezapouramini2323@example.com>
2026-07-03 10:51:41 +0330

    feat: add contants/routes.js file

---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+const ROUTES = {
+    AUTH: {
+        REGISTER: "/api/v1/auth/register",
+        VERIFY_EMAIL: "/api/v1/auth/verify-email",
+        LOGIN: "/api/v1/auth/login",
+        LOGOUT: "/api/v1/auth/logout",
+        REFRESH_TOKEN: "/api/v1/auth/refresh-token",
+    }
+}

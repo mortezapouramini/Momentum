a673d028439e6986a3984c948e0c421bfc004076
mortezapouramini <mortezapouramini2323@example.com>
2026-07-05 09:48:27 +0330

    fix: change  ES module syntax to common js/update refresh token cookie path

--- src/config/cookie-config.js
+++ src/config/cookie-config.js
@@ -1,16 +1,18 @@
-export const cookieOptions = {
-    uuid : {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        maxAge: 5 * 60 * 1000,
-        path : '/api/v1/auth/verify-email'
-    },
-    refreshToken : {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        path : '/api/v1/auth/refresh-token'
-    }
-}
+const cookieOptions = {
+  uuid: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 5 * 60 * 1000,
+    path: "/api/v1/auth/verify-email",
+  },
+  refreshToken: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/api/v1/auth",
+  },
+};
+
+module.exports = { cookieOptions };

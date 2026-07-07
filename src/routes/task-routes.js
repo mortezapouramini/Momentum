89ed2982e956ab3e9a9197d97cb2d6dc21e39be5
mortezapouramini <mortezapouramini2323@example.com>
2026-07-07 10:53:01 +0330

    feat : add task delete route

--- src/routes/task-routes.js
+++ src/routes/task-routes.js
@@ -3,7 +3,8 @@ const router = require("express").Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 const taskController = require("../controllers/task-controller");
 
-router.post("/", authMiddleware.authAccessToken, taskController.createTask);
+router
+  .post("/", authMiddleware.authAccessToken, taskController.createTask)
+  .delete("/:id", authMiddleware.authAccessToken , taskController.deleteTask);
 
-
-module.exports = router
+module.exports = router;

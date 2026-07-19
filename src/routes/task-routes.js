2b27b355f46322693db8bf554b3b8164ce6e1f48
mortezapouramini <mortezapouramini2323@example.com>
2026-07-19 08:30:18 +0330

    feat : implement update task validation

--- src/routes/task-routes.js
+++ src/routes/task-routes.js
@@ -3,13 +3,26 @@ const router = require("express").Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 const taskController = require("../controllers/task-controller");
 const { validate } = require("../middlewares/validator-middleware");
-const { createTaskSchema } = require("../validations/task-validation");
+const {
+  createTaskSchema,
+  updateTaskSchema,
+} = require("../validations/task-validation");
 
 router
-  .post("/", authMiddleware.authAccessToken, validate(createTaskSchema) , taskController.createTask)
+  .post(
+    "/",
+    authMiddleware.authAccessToken,
+    validate(createTaskSchema),
+    taskController.createTask,
+  )
   .delete("/:id", authMiddleware.authAccessToken, taskController.deleteTask)
-  .patch("/:id", authMiddleware.authAccessToken, taskController.updateTask)
+  .patch(
+    "/:id",
+    authMiddleware.authAccessToken,
+    validate(updateTaskSchema),
+    taskController.updateTask,
+  )
   .get("/:id", authMiddleware.authAccessToken, taskController.getSingleTask)
-  .get("/" , authMiddleware.authAccessToken , taskController.getTasks)
+  .get("/", authMiddleware.authAccessToken, taskController.getTasks);
 
 module.exports = router;
--- src/validations/task-validation.js
+++ src/validations/task-validation.js
@@ -1,17 +1,17 @@
 const { object, string, number, date } = require("yup");
 
-const titleField = string("Title must be string")
+let titleField = string("Title must be string")
   .trim()
   .max(50, "Title must be at most 50 characters");
-const descriptionField = string("Description must be string")
+let descriptionField = string("Description must be string")
   .trim()
   .max(1000, "Description must be at most 1000 characters");
-const priorityField = string("Priority must be string")
+let priorityField = string("Priority must be string")
   .trim()
   .transform((val) => val.toLowerCase())
   .oneOf(["high", "medium", "low"], "Priority can be low, medium or high");
 
-const statusField = string("Priority must be string")
+let statusField = string("Priority must be string")
   .trim()
   .transform((val) => val.toLowerCase())
   .oneOf(
@@ -19,9 +19,9 @@ const statusField = string("Priority must be string")
     "Priority can be pending, in-progress or done",
   );
 
-const dueDateField = date().typeError("Invalid date");
+let dueDateField = date().typeError("Invalid date");
 
-const createTaskSchema = object({
+let createTaskSchema = object({
   title : titleField.required('Title is required'),
   description : descriptionField,
   priority : priorityField,
@@ -29,8 +29,16 @@ const createTaskSchema = object({
   dueDate : dueDateField.required("dueDate is required"),
 });
 
+let updateTaskSchema = object({
+  title : titleField,
+  description : descriptionField,
+  priority : priorityField,
+  status : statusField,
+  dueDate : dueDateField,
+});
 
 
 module.exports = {
-    createTaskSchema
+    createTaskSchema,
+    updateTaskSchema
 }

b31d2d0049f9b7d79bbab2d879dfc4dec7f3969e
mortezapouramini <mortezapouramini2323@example.com>
2026-08-03 10:46:34 +0330

    feat : implement pino logger

---
+++ src/config/logger.config.js
@@ -0,0 +1,19 @@
+const pino = require("pino");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const logger = pino({
+  level: isDev ? "debug" : "info",
+  transport: isDev
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          ignore: "pid,hostname",
+        },
+      }
+    : undefined,
+});
+
+module.exports = { logger };
